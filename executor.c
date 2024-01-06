208bb87645bd9b229130ef4e043b95dd2dd1d013
kali <osemafadhel41@gmail.com>
2024-01-06 20:41:18 +0100

    lexer

--- executor.c
+++ executor.c
@@ -6,7 +6,7 @@
 /*   By: ofadhel <ofadhel@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/28 17:38:14 by ofadhel           #+#    #+#             */
-/*   Updated: 2024/01/04 19:37:48 by ofadhel          ###   ########.fr       */
+/*   Updated: 2024/01/06 20:40:41 by ofadhel          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -30,7 +30,7 @@ void	here_doc(t_mini *mini, char *delimeter)
 	char	*line;
 	int		fd;
 
-	fd = open("tmp.txt", O_WRONLY | O_TRUNC | O_CREAT, 0644);
+	fd = open(".tmp.txt", O_WRONLY | O_TRUNC | O_CREAT, 0644);
 	while (1)
 	{
 		line = readline("> ");
@@ -44,7 +44,7 @@ void	here_doc(t_mini *mini, char *delimeter)
 		free(line);
 	}
 	close(fd);
-	mini->fdin = open("tmp.txt", O_RDONLY);
+	mini->fdin = open(".tmp.txt", O_RDONLY);
 }
 
 void	update_fd(t_mini *mini, t_cmds *current_cmd)
@@ -95,13 +95,13 @@ void execute(t_mini *mini)
 	while (current_cmd != NULL)  //missing case like like ls >file txt | wc -l //the fdin for wc need to be the fdout of ls
 	{
 		update_fd(mini, current_cmd);
-		if (mini->fdin == -2 && cmd_count == 0)
-			mini->fdin = dup(tmpin);
+		/*if (mini->fdin == -2 && cmd_count == 0)
+			mini->fdin = dup(tmpin);*/ // gia c'e su
 		if (current_cmd->in == 0 && cmd_count != 0 && pipe_flag == 0)
-			mini->fdin = dup(tmpin);
+			mini->fdin = dup(tmpin); // cambiare il fdin con un fd vuoto
 		dup2(mini->fdin, 0);
 		close(mini->fdin);
-		if	(current_cmd->out == 0 || (current_cmd->next == NULL && current_cmd->out == 0))
+		if	(current_cmd->next == NULL && current_cmd->out == 0)
 			mini->fdout = dup(tmpout);
 		if (mini->cmds_count >= 1 && current_cmd->out == 0 && current_cmd->next != NULL)  // to check
 		{
@@ -121,10 +121,15 @@ void execute(t_mini *mini)
 			executor(mini, current_cmd); // Call the executor with the current command
 			exit(1);
 		}
+		else if (ret < 0)
+		{
+			printf("Error forking\n");
+			exit(1);
+		}
 		current_cmd = current_cmd->next;
 		cmd_count++;
 		if (mini->here_doc_flag == 1)
-			unlink("tmp.txt");
+			unlink(".tmp.txt");
 	}
 	// Restore in/out defaults
 	dup2(tmpin, 0);
